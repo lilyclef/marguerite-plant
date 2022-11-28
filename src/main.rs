8985301beb9be6b4597dca7e392773474be90566
Yuri Yamamoto <lilyclef@gmail.com>
2022-11-29 02:09:38 +0900

    #1 POST & GET実装しました

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, post, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Board {
     id: String,
-    content: String,
+    creator_id: String,
+    card_list: Vec<Card>,
+    board_title: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Card {
+    id: String,
+    board_id: String,
     writer_id: String,
+    writer_name: String,
+    contents: String
+}
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct BoardInput {
+    creator_id: String,
+    board_title: String
 }
 
 #[get("/board/{board_id}")]
@@ -13,14 +30,49 @@ async fn get_board(board_id: web::Path<String>) -> impl Responder {
     println!("get_board {}", board_id.to_string());
     HttpResponse::Ok().json(Board {
         id: board_id.to_string(),
-        content: String::from("test"),
-        writer_id: String::from("test")
+        creator_id: String::from("test"),
+        card_list: [Card{
+            id: "00000000".to_string(),
+            board_id: board_id.to_string(),
+            writer_id: "00000000".to_string(),
+            writer_name: "Yuri".to_string(),
+            contents: "Thank you!".to_string()
+        }].to_vec(),
+        board_title: String::from("Alice")
     })
+}
+
+#[get("/card/{card_id}")]
+async fn get_card(card_id: web::Path<String>) -> impl Responder {
+    println!("get_card {}", card_id.to_string());
+    HttpResponse::Ok().json(Card{
+            id: card_id.to_string(),
+            board_id: "00000000".to_string(),
+            writer_id: "00000000".to_string(),
+            writer_name: "Yuri".to_string(),
+            contents: "Thank you!".to_string()
+    })
+}
+
+
+#[post("/card")]
+async fn post_card(cardinfo: web::Json<Card>) -> impl Responder {
+    println!("post_card");
+    HttpResponse::Ok().json({})
+}
+
+#[post("/board")]
+async fn post_board(info: web::Json<BoardInput>) -> impl Responder {
+    println!("post_board");
+    HttpResponse::Ok().json(Board{id: "00000000".to_string(),
+                                  board_title: "Hi".to_string(),
+                                  card_list: [].to_vec(),
+                                  creator_id: "00000000".to_string()})
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_board))
+    HttpServer::new(|| App::new().service(get_board).service(get_card).service(post_card).service(post_board))
         .bind("127.0.0.1:8080")?
         .run()
         .await
